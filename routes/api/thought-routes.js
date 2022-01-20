83bfd04c4b2aab2f4a7a6f642c6e938b0f7e4c29
somdobomk <somdobomk@gmail.com>
2022-01-19 19:29:41 -0600

    update

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,20 +1,17 @@
 const router = require('express').Router();
 const { getAllThoughts, getThoughtById, createThought, updateThought, deleteThought, deleteReaction, addReaction} = require('../../controllers/thought-controller');
 
-// router with no id
 router
   .route('/')
   .get(getAllThoughts)
   .post(createThought);
 
-// router with thought it
 router
   .route('/:id')
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
 
-// router with reaction
 router
   .route('/:thoughtId/reactions')
   .post(addReaction)
